b1f3e5356c06e6c8603b163a1eee97014808e1fc
Sayan <34585895+Sayan0Programmer@users.noreply.github.com>
2018-02-11 02:54:50 +0530

    Add files via upload

---
+++ ultrasonic_hr/ultrasonic_hr.ino
@@ -0,0 +1,19 @@
+const int trig=13,echo=12;
+void setup(){
+	pinMode(trig,OUTPUT);
+	pinMode(echo,INPUT);
+	Serial.begin(9600);
+}
+void loop(){
+	long duration,distance;
+	digitalWrite(trig,LOW);
+	delayMicroseconds(2);
+	digitalWrite(trig,HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trig,LOW);
+	duration=pulseIn(echo,HIGH);
+	distance=(duration/2)/29.1;
+	Serial.print(distance);
+	Serial.println(" cm");
+	delay(1000);
+}
