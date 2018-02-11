ce80809138dadae78fc1b1b8629938e232c2a07e
Sayan <34585895+Sayan0Programmer@users.noreply.github.com>
2018-02-11 16:29:31 +0530

    Add files via upload

---
+++ sensor_rf433_rxtx/sensor_rf_rxd/sensor_rf_rxd.ino
@@ -0,0 +1,42 @@
+int i,good,k;
+byte d;//data
+const int ip=3;
+void setup(){
+  attachInterrupt(1,data_incoming,RISING);
+  pinMode(ip,INPUT);
+  Serial.begin(115200);
+}
+void loop(){
+
+}
+void data_incoming(){
+  for(i=0;i<100;i++){
+    delayMicroseconds(20);
+    good=1;
+    if(digitalRead(ip)==LOW){
+      good=0;
+      i=100;
+    }
+  } 
+  if(good==1){
+    detachInterrupt(1);
+    while(1){
+      if(digitalRead(ip)==LOW){
+        delayMicroseconds(750);
+        for(i=0;i<8;i++){
+          if(digitalRead(ip)==HIGH)
+            bitWrite(d,i,1);
+          else
+            bitWrite(d,i,0);
+          delayMicroseconds(1000);
+        }
+        if(d=='#')
+          Serial.println("");
+        else
+          Serial.print(char(d));
+        break;//secondtwhile
+      }//low kickoff
+    }//second while
+  }//good check
+  attachInterrupt(1,data_incoming,RISING);
+}//routine
